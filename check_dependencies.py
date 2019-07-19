0159c65d6abd5b82dda56a37a3774daa304b4bef
Jerzy Kowalski <jerzy.kowalski@stxnext.pl>
2019-07-19 11:34:38 +0200

    Add script for checking dependencies

---
+++ check_dependencies.py
@@ -0,0 +1,9 @@
+import skimage
+import sklearn
+import matplotlib
+
+
+print(f"{sklearn.__name__.title()} is installed ✓")
+print(f"{matplotlib.__name__.title()} is installed ✓")
+print(f"{skimage.__name__.title()} is installed ✓")
+print(f"You're ready for ML hacking!")
